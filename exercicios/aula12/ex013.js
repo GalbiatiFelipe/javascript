d330d31bd7596bfb2aa34dc70b1e21445648f1cd
Felipe de Oliveira Galbiati <gaalbiatii.felipe@gmail.com>
2025-03-19 20:13:58 -0300

    cond aninhadas e multiplas

---
+++ exercicios/aula12/ex013.js
@@ -0,0 +1,36 @@
+
+var diasem = 3
+/*
+domingo = 0
+seg = 1
+ter = 2
+qua = 3
+qui = 4
+sex = 5
+sab = 6
+*/
+switch(diasem) {
+    case 0:
+        console.log('Domingo')
+        break
+    case 1:
+        console.log('Segunda')
+        break
+    case 2:
+        console.log('Terça')
+        break
+    case 3: 
+        console.log('Quarta')
+        break
+    case 4:
+        console.log('Quinta')
+        break
+    case 5:
+        console.log('Sexta')
+        break
+    case 6:
+        console.log('Sábado')
+        break
+    default:
+        console.log('Dia inválido')
+}
