85351e69a7c0fc9e32b71684d9606260e1c6d92d
Felipe de Oliveira Galbiati <gaalbiatii.felipe@gmail.com>
2025-04-03 20:33:23 -0300

    arrays

---
+++ exercicios/aula15/ambiente.js
@@ -0,0 +1,4 @@
+var num = [3, 13, 9]
+num[3] = 2
+num.sort()
+console.log(`A variavel possui estes valores: ${num}`)
---
+++ exercicios/aula15/vetor.js
@@ -0,0 +1,6 @@
+let valores = [3, 4, 5, 9, 2, 7]
+
+for(let pos in valores) {
+    valores.sort()
+    console.log(`A posição ${pos} tem o valor de ${valores[pos]}`)
+}
