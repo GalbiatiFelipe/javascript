2d4fafc613fb80ba432eadc358ce26ff24b50ce7
Felipe de Oliveira Galbiati <gaalbiatii.felipe@gmail.com>
2025-04-09 19:08:20 -0300

    final do ex

--- pratica/functions e arrays/script.js
+++ pratica/functions e arrays/script.js
@@ -15,7 +15,7 @@ function numero(n) {
  cria uma função 'numero' com parametro qualquer, se esse parametro que vai receber um numero for maior ou igual a um & esse parametro for menor ou igual a 100, o retorno será true, se não sera false.
 */
 
-function nalista(l, n) {
+function nalista(n, l) {
     if (l.indexOf(Number(n)) != -1) {
         return true
     } else {
@@ -29,14 +29,19 @@ l.indexof(number(n)) significa que o numero digitado vai ser procurado na lista.
 */
 function adicionar() {
     if (numero(num.value) && !nalista(num.value, valores)) {
-       
+       valores.push(Number(num.value)) //adiciona dentro da let 'valores' o número digitado no input 'num'.
+       let item = document.createElement('option') // uma variavel que cria a tag 'option' no HTML.
+       item.text = `O valor ${num.value} foi adicionado` //adiciona um texto a essa tag.
+       lista.appendChild(item) //faz o texto aparecer no local que a let 'lista' está se referindo, nesse caso a tag 'select'.
     } else {
         alert('Número inválido ou ja adicionado.')
     }
+    num.value = ''
+    num.focus()
     
     
 }
 
 /*
-função adicionar() vinculada a uma ação onclick no html, nesse caso ela verifica que se o 'return' das funções 'numero' e 'nalista' foram verdadeiros (true)
+função adicionar() vinculada a uma ação onclick no html, nesse caso ela verifica que se o 'return' das funções 'numero' e 'nalista' foram verdadeiros (true).
 */
