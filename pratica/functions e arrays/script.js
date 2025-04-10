ad5a9f0c450bd63fe160edccfe923f9c86b329ca
Felipe de Oliveira Galbiati <gaalbiatii.felipe@gmail.com>
2025-04-10 09:23:02 -0300

    estudos

--- pratica/functions e arrays/script.js
+++ pratica/functions e arrays/script.js
@@ -12,7 +12,7 @@ function numero(n) {
 }
 
 /*
- cria uma função 'numero' com parametro qualquer, se esse parametro que vai receber um numero for maior ou igual a um & esse parametro for menor ou igual a 100, o retorno será true, se não sera false.
+ cria uma função 'numero' com parametro qualquer, se esse parametro que vai receber um numero for maior ou igual a 1 & esse parametro for menor ou igual a 100, o retorno será true, se não sera false.
 */
 
 function nalista(n, l) {
@@ -36,8 +36,8 @@ function adicionar() {
     } else {
         alert('Número inválido ou ja adicionado.')
     }
-    num.value = ''
-    num.focus()
+    num.value = '' // ele está depois do código portanto após a função ser executada o num.value será vazio
+    num.focus() // ele está depois do código portanto após a função ser executada o mouse vai 'clicar' na caixa automaticamente
     
     
 }
@@ -45,3 +45,47 @@ function adicionar() {
 /*
 função adicionar() vinculada a uma ação onclick no html, nesse caso ela verifica que se o 'return' das funções 'numero' e 'nalista' foram verdadeiros (true).
 */
+
+function finalizador() {
+    if (valores.length == 0) {
+        window.alert('Adicione números antes de finalizar')
+    } else {
+        let quant = valores.length
+        let maior = valores[0]
+        let menor = valores[0]
+        let soma = 0
+        let media = 0
+
+            /*
+            for (let pos = 1; valores[pos] > maior; pos++) {
+                maior = valores[pos]
+            }
+            let pos = 1
+            while (valores[pos] < menor) {
+                menor = valores[pos]
+                pos--
+            }
+            */
+        
+        for (let pos in valores) { // cria uma variavel que indica o índice de cada número da variavel 'valores'.
+            soma += valores[pos] // 'soma' é igual a zero, por estar dentro do for serão somados todos os números de 'valores'.
+            if (valores[pos] > maior) { 
+                maior = valores[pos] // se a posição for maior que o índice 0 (indicado pela variavel 'let maior') let maior recebe o número localizado no índice 0.
+            }
+            if (valores[pos] < menor) {
+                menor = valores[pos] // se a posição for menor que o índice 0, let menor recebe o número localizado no índice 0.
+            } 
+        }
+        media = soma / quant // sabendo a soma fica simples fazer a operção de média.
+    /*
+    No comentario acima temos a versão básica do código acima.
+    */
+
+        res.innerHTML = ''
+        res.innerHTML += `<p>Ao todos temos ${quant} números adicionados:</p>`
+        res.innerHTML += `<p>*O maior número encontrado foi o ${maior}.</p>`
+        res.innerHTML += `<p>*O menor número encontrado foi o ${menor}.</p>`
+        res.innerHTML += `<p>*A soma de todos os valores é ${soma}.</p>`
+        res.innerHTML += `<p>*A média de todos os valores é ${media}.</p>`
+    }
+}
