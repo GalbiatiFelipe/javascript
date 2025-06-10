d7d32f572935c7a99131717e3ca7c07f3032575e
Felipe de Oliveira Galbiati <gaalbiatii.felipe@gmail.com>
2025-06-09 23:51:58 -0300

    2

---
+++ pratica/poo/objetos.js
@@ -0,0 +1,13 @@
+// praticando POO
+
+class animal {
+    constructor(nome, raca) {
+        this.nome = nome
+        this.raca = raca
+    }
+
+}
+
+const cachorro = new animal('cleber', 'bulldog')
+
+console.log(cachorro)
